6cc40bfe0fed347e2d0bbf595cf004b57a1a25b4
Ines Petersen <73462125+inesC0des@users.noreply.github.com>
2026-08-29 09:40:40 +0200

    Update server.js
    Ping/Pong Synchronisation

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -9,22 +8,22 @@ const io = new Server(server, { cors: { origin: '*' } });
 
 let timerState = { duration: 0, endTime: null, isRunning: false };
 
-// NEW: Helper function that sends the Server's true clock time
 function broadcastState() {
-  io.emit('timer_state', { ...timerState, serverTime: Date.now() });
+  io.emit('timer_state', { ...timerState });
 }
 
 io.on('connection', (socket) => {
-  socket.emit('timer_state', { ...timerState, serverTime: Date.now() });
+  socket.emit('timer_state', { ...timerState });
   io.emit('user_count', io.engine.clientsCount);
+
+  // --- NEW: The Ping/Pong time synchronizer ---
+  socket.on('request_ping', (clientTime) => {
+    socket.emit('response_pong', { clientTime: clientTime, serverTime: Date.now() });
+  });
 
   socket.on('start_timer', (minutes) => {
     const durationMs = minutes * 60 * 1000;
-    timerState = {
-      duration: durationMs,
-      endTime: Date.now() + durationMs,
-      isRunning: true
-    };
+    timerState = { duration: durationMs, endTime: Date.now() + durationMs, isRunning: true };
     broadcastState();
   });
 
